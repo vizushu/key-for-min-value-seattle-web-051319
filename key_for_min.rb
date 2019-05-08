3936e6804c982764392fc53d95b6475cf4675880
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-08 17:40:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil
 
 
- end 	  name_hash.each do |key,value|
+  name_hash.each do |key,value|
     if lowest_value == 0 || value < lowest_value
       lowest_value = value
       lowest_key = key
