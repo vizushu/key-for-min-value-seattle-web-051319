3018ad1299f10e0765368af3bfcba2518b526fe3
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-08 17:29:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  return 
   name_hash.collect do |name, value|
     if value == 1
       return name
